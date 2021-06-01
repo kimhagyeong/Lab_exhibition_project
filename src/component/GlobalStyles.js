fbbdbf650a3586c2cdebf8213a7038ec5197e7ad
kimhagyeong <abcd358583@gmail.com>
2021-06-02 00:06:42 +0900

    add animation

--- src/component/GlobalStyles.js
+++ src/component/GlobalStyles.js
@@ -12,7 +12,17 @@ const globalStyle = createGlobalStyle`
     body{
         position:relative;
     }
-    
+    section{
+        animation: fadein 0.5s ease 1;
+    }
+    @keyframes fadein {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
 `;
 
 export default globalStyle;
