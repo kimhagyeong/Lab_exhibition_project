35c9fbb9bd706fa29bd2b06f0e0c88ab418ffeee
whitepurple <rofetist@gmail.com>
2021-08-04 19:41:40 +0900

    fix key

--- server/labproject/settings.py
+++ server/labproject/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b2ar5=$ovc=ni51p=u_e_2ib^w^vz!x@t__$@*sg-mhx_utfjn'
-
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-b2ar5=$ovc=ni51p=u_e_2ib^w^vz!x@t__$@*sg-mhx_utfjn')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
